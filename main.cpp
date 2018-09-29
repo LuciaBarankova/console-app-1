9ecaff404e96df21168098598a791f5afc58a480
LuciaBarankova <lucia.lula.barankova@gmail.com>
2018-09-29 19:20:37 +0200

    Spocitavanie znakov viacriadkoveho vstupu

--- main.cpp
+++ main.cpp
@@ -2,10 +2,35 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-	std::cout << "Hello World!" << std::endl;
+	//cout << argc << endl;
+	//cout << string(argv[0]) << endl;
+	//cout << string(argv[1]) << endl;
+	//cout << "Hello World!" << endl;
+
+	if (argc == 1) return 0;
+	else if (argc == 2)
+	{
+		string input;
+		string iinput;
+		int num = 0;
+		while (getline(cin, iinput))
+		{
+			input.append(iinput + "\n");
+			num++;
+		}
+		if (string(argv[1]) == "-c")
+		{
+			int length = input.length() - num;
+			cout << length << endl;
+		}
+	}
+
+
 
 	return 0;
 }
