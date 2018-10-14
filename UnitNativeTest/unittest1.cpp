774bc0e42eec89c5cc8fb7929d804b2bb05662f3
LuciaBarankova <lucia.lula.barankova@gmail.com>
2018-10-14 15:25:19 +0200

    Pridany project Windows Desktop

--- UnitNativeTest/unittest1.cpp
+++ UnitNativeTest/unittest1.cpp
@@ -9,17 +9,17 @@
 TEST_CASE("Testovanie funkcnosti counter funkcii", "TestCounter")
 {
 	string test1 = "abc\n";
-	REQUIRE(counter_char(test1) == 3);
-	REQUIRE(counter_word(test1) == 1);
-	REQUIRE(counter_line(test1) == 1);
+	REQUIRE(Spocitaj<_char>(test1) == 3);
+	REQUIRE(Spocitaj<_word>(test1) == 1);
+	REQUIRE(Spocitaj<_line>(test1) == 1);
 
 	string test2 = ". abc\na..bc\n";
-	REQUIRE(counter_char(test2) == 10);
-	REQUIRE(counter_word(test2) == 3);
-	REQUIRE(counter_line(test2) == 2);
+	REQUIRE(Spocitaj<_char>(test2) == 10);
+	REQUIRE(Spocitaj<_word>(test2) == 3);
+	REQUIRE(Spocitaj<_line>(test2) == 2);
 
 	string test3 = " abc\nabc   Dfg\nabc ...\n";
-	REQUIRE(counter_char(test3) == 20);
-	REQUIRE(counter_word(test3) == 4);
-	REQUIRE(counter_line(test3) == 3);
+	REQUIRE(Spocitaj<_char>(test3) == 20);
+	REQUIRE(Spocitaj<_word>(test3) == 4);
+	REQUIRE(Spocitaj<_line>(test3) == 3);
 }
